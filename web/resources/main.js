0f28b76379e590828fa02b4a10b7ab8e1ef5ab1f
XSA_DEV <XSA_DEV@unknown>
2019-02-27 13:09:26 +0000

    tests

---
+++ web/resources/main.js
@@ -0,0 +1,11 @@
+sap.ui.define(['sap/m/Text'], function(Text) {
+    new Text({
+        text: "test label 1"
+    }).placeAt("content");
+});
+
+sap.ui.define(['sap/m/Text'], function(Text) {
+    new Text({
+        text: "test label 2"
+    }).placeAt("content");
+});
